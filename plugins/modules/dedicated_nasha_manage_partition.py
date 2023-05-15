15bdefb7349537d8ae2845dbbcde0407d3814164
Guillaume Gambs <guillaume.gambs@digimind.com>
2023-05-15 15:09:22 +0200

    Fix typo  + add Return Doc

--- plugins/modules/dedicated_nasha_manage_partition.py
+++ plugins/modules/dedicated_nasha_manage_partition.py
@@ -11,7 +11,7 @@ DOCUMENTATION = '''
 module: dedicated_nasha_manage_partition
 short_description: Create a nasha partition.
 description:
-    - create a nasha partition with specified ACL.
+    - Create a nasha partition with specified ACL.
 author: Synthesio SRE Team
 requirements:
     - ovh >= 0.5.0
@@ -30,12 +30,12 @@ options:
         - The size of the partition you want to create. Must be >= 10 Gb
     nas_protocol:
         required: true
-        choices: ['NFS','CIFS','NFS_CIFS']
+        choices: ['NFS', 'CIFS', 'NFS_CIFS']
         description:
         - The protocol of the partition
     nas_partition_acl_type:
         required: false
-        choices: ['readonly','readwrite']
+        choices: ['readonly', 'readwrite']
         description:
         - The type of the ACL you want to create
     nas_partition_acl:
@@ -45,24 +45,30 @@ options:
 '''
 
 EXAMPLES = '''
-- name : Create a nasha partition with specified ACL
-    synthesio.ovh.dedicated_nasha_manage_partition:
-    nas_service_name: {{ nas_service_name }}
-    nas_partition_name: {{ nas_partition_name }}
+- name: Create a nasha partition with specified ACL
+  synthesio.ovh.dedicated_nasha_manage_partition:
+    nas_service_name: "{{ nas_service_name }}"
+    nas_partition_name: "{{ nas_partition_name }}"
     nas_partition_size: 10
     nas_protocol: NFS
     nas_partition_acl_type: readwrite
     nas_partition_acl:
-    - XX.XX.XX.XX/32
-    - XX.XX.XX.XX/32
+      - XX.XX.XX.XX/32
+      - XX.XX.XX.XX/32
 '''
 
-RETURN = ''' # '''
+RETURN = '''
+changed:
+    description: Indicates whether the module made any changes.
+    type: bool
+'''
 
-from ansible_collections.synthesio.ovh.plugins.module_utils.ovh import ovh_api_connect, ovh_argument_spec
+from ansible_collections.synthesio.ovh.plugins.module_utils.ovh import (
+    ovh_api_connect, ovh_argument_spec
+)
 
 try:
-    from ovh.exceptions import APIError
+    from ovh.exceptions import APIError, ResourceNotFoundError
     HAS_OVH = True
 except ImportError:
     HAS_OVH = False
@@ -93,27 +99,43 @@ def run_module():
     nas_partition_acl_type = module.params['nas_partition_acl_type']
     nas_protocol = module.params['nas_protocol']
 
-    ## Create partitionnot
-
+    # Create partition
     if not module.check_mode:
         try:
-            client.post('/dedicated/nasha/{0}/partition'.format(nas_service_name), size=nas_partition_size, partitionName=nas_partition_name, protocol=nas_protocol)
+            client.post(
+                '/dedicated/nasha/{0}/partition'.format(nas_service_name),
+                size=nas_partition_size,
+                partitionName=nas_partition_name,
+                protocol=nas_protocol
+            )
         except APIError as api_error:
-            module.fail_json(msg="Failed to create partition: %s" % e)           
+            module.fail_json(msg="Failed to create partition: %s" % api_error)
     else:
         try:
             client.get('/dedicated/nasha/{0}'.format(nas_service_name))
-        except APIError as api_error:
-            module.fail_json(msg="Failed to get partition: %s" % e)
 
-    ## Set partition ACL
+        except APIError as api_error:
+            module.fail_json(msg="Failed to get partition: %s" % api_error)
+
+    # Set partition ACL
     if not module.check_mode:
-        for ip_acl in nas_partition_acl:    
+        for ip_acl in nas_partition_acl:
             try:
-                client.post('/dedicated/nasha/{0}/partition/{1}/access'.format(nas_service_name, nas_partition_name), ip=ip_acl, type=nas_partition_acl_type)
-                module.exit_json(msg="IP {} has been added to ACL of {} partition".format(ip_acl, nas_partition_name), changed=True)
+                client.post(
+                    '/dedicated/nasha/{0}/partition/{1}/access'.format(
+                        nas_service_name, nas_partition_name
+                    ),
+                    ip=ip_acl,
+                    type=nas_partition_acl_type
+                )
+                module.exit_json(
+                    msg="IP {} has been added to ACL of {} partition".format(
+                        ip_acl, nas_partition_name
+                    ),
+                    changed=True
+                )
             except APIError as api_error:
-                module.fail_json(msg="Failed to set partition ACL: %s" % e)
+                module.fail_json(msg="Failed to set partition ACL: %s" % api_error)
 
 
 def main():
